04619f0bea03c671565772dc0b6f07e5e3e16818
Rubén Rangel <rrangel@sproutloud.com>
2025-06-19 11:09:54 -0500

    refactor(service): put server in app.ts

--- src/tests/e2e/status.test.ts
+++ src/tests/e2e/status.test.ts
@@ -9,8 +9,8 @@ describe('Status endpoint', () => {
     let server: Server;
 
     beforeAll(() => {
-        const dbUrl = process.env.DB_URL!;
-        const port = process.env.PORT!;
+        const dbUrl: string = process.env.DB_URL || 'mongodb://127.0.0.1:27017/db_orders_test';
+        const port: number = Number(process.env.PORT) || 3003;
         server = createServer(port, dbUrl)
     });
 
