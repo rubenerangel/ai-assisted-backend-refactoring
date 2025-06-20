14a23afa623d03223b6e03d9e2c9379a5f65a00d
Rubén Rangel <rrangel@sproutloud.com>
2025-06-20 12:56:30 -0500

    green: Allows valid addresses

--- src/domain/valueObject.ts
+++ src/domain/valueObject.ts
@@ -8,3 +8,15 @@ export class PositiveNumber {
         return new PositiveNumber(value);
     }
 }
+
+export class Address {
+    private constructor(readonly value: string) {
+    }
+
+    static create(value: string): Address {
+        if (!value || value.trim() === '') {
+            throw new Error('Address cannot be empty');
+        }
+        return new Address(value);
+    }
+}
--- src/tests/unit/valueObjects.test.ts
+++ src/tests/unit/valueObjects.test.ts
@@ -1,4 +1,4 @@
-import {PositiveNumber} from "../../domain/valueObject";
+import {Address, PositiveNumber} from "../../domain/valueObject";
 
 describe('A positive number', () => {
     it('Allows positive values', () => {
@@ -9,4 +9,20 @@ describe('A positive number', () => {
     it('Does not allow negative values', () => {
         expect(() => PositiveNumber.create(-1)).toThrow('Value must be a positive number');
     });
+})
+
+describe('An address', () => {
+    it('Allows valid addresses', () => {
+        const anAddress = Address.create('123 Main St, Springfield, USA');
+        expect(anAddress.value).toBe('123 Main St, Springfield, USA');
+    });
+
+    it('Does not allow empty addresses', () => {
+        expect(() => Address.create('')).toThrowError('Address cannot be empty');
+        expect(() => Address.create('   ')).toThrow('Address cannot be empty');
+    });
+
+    // it('Does not allow addresses with only spaces', () => {
+    //
+    // });
 })
