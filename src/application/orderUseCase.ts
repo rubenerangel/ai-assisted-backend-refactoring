cdaee0c1075cfeef020e7d9a2bb249b9bbff2271
Rubén Rangel <rrangel@sproutloud.com>
2025-07-02 16:18:19 -0500

    refactor: encapsulate use case functions with class

---
+++ src/application/orderUseCase.ts
@@ -0,0 +1,85 @@
+import {DiscountCode, OrderStatus} from "../domain/models";
+import {OrderRepository} from "../domain/repositories";
+import {Address, Id, OrderLine, PositiveNumber} from "../domain/valueObject";
+import {Order} from "../domain/entities";
+import {DomainError} from "../domain/error";
+
+type RequestOrder = {
+    items: {
+        productId: string;
+        quantity: number;
+        price: number;
+    }[];
+    shippingAddress: string;
+    discountCode: DiscountCode;
+}
+type RequestOrderUpdate = {
+    id: string;
+    shippingAddress?: string;
+    status: OrderStatus;
+    discountCode: DiscountCode;
+}
+
+export class OrderUseCase {
+    constructor(private repo: OrderRepository) {}
+
+    async createOrder(requestOrder: RequestOrder) {
+        const orderLines = requestOrder.items.map((item: any) => (
+            new OrderLine(
+                Id.from(item.productId),
+                PositiveNumber.create(item.quantity),
+                PositiveNumber.create(item.price)
+            )
+        ))
+        const order = Order.create(orderLines, Address.create(requestOrder.shippingAddress), requestOrder.discountCode);
+
+        await this.repo.save(order);
+        return `Order created with total: ${order.calculatesTotal().value}`;
+    }
+
+    async updateOrder(requestOrderUpdate: RequestOrderUpdate) {
+        const order = await this.repo.findById(Id.from(requestOrderUpdate.id)) as Order;
+
+        if (!order) {
+            throw new DomainError(`Order not found`)
+        }
+        if (requestOrderUpdate.shippingAddress) {
+            order.updateShippingAddress(Address.create(requestOrderUpdate.shippingAddress));
+        }
+        if (requestOrderUpdate.status) {
+            order.updateStatus(requestOrderUpdate.status);
+        }
+        if (requestOrderUpdate.discountCode) {
+            order.updateDiscountCode(requestOrderUpdate.discountCode);
+        }
+
+        await this.repo.save(order);
+        return `Order updated. New status: ${order.toDTO().status}`;
+    }
+
+    async getAllOrders() {
+        const orders = await this.repo.findAll();
+        return orders.map(order => order.toDTO());
+    }
+
+    async completeOrder(id: string) {
+        const order = await this.repo.findById(Id.from(id)) as Order;
+
+        if (!order) {
+            throw new DomainError(`Order not found to complete with id: ${id}`)
+        }
+        order.complete();
+        await this.repo.save(order);
+        return `Order with id ${order.toDTO().id} completed`;
+    }
+
+    async deleteOrder(id: string) {
+        const order = await this.repo.findById(Id.from(id)) as Order;
+
+        if (!order) {
+            throw new DomainError('Order not found')
+        }
+        await this.repo.delete(order.getId())
+        return 'Order deleted';
+    }
+}
--- src/infrastructure/controllers/orderController.ts
+++ src/infrastructure/controllers/orderController.ts
@@ -1,75 +1,7 @@
 import {Request, Response} from 'express';
-import {Address, Id, OrderLine, PositiveNumber} from "../../domain/valueObject";
-import {Order} from "../../domain/entities";
 import {DomainError} from "../../domain/error";
 import {Factory} from "../../factory";
-import {OrderRepository} from "../../domain/repositories";
-
-async function createOrderUseCase(requestOrder:any, repo: OrderRepository) {
-    const orderLines = requestOrder.items.map((item: any) => (
-        new OrderLine(
-            Id.from(item.productId),
-            PositiveNumber.create(item.quantity),
-            PositiveNumber.create(item.price)
-        )
-    ))
-    const order = Order.create(orderLines, Address.create(requestOrder.shippingAddress), requestOrder.discountCode);
-
-    await repo.save(order);
-    return `Order created with total: ${order.calculatesTotal().value}`;
-}
-
-async function getAllOrdersUseCase(repo: OrderRepository) {
-    const orders = await repo.findAll();
-    return orders.map(order => order.toDTO());
-}
-
-async function updateOrderUseCase(repo: OrderRepository, requestOrderUpdate:any) {
-    const order = await repo.findById(Id.from(requestOrderUpdate.id)) as Order;
-
-    if(!order) {
-        throw new DomainError(`Order not found`)
-    }
-    if (requestOrderUpdate.shippingAddress) {
-        order.updateShippingAddress(Address.create(requestOrderUpdate.shippingAddress));
-    }
-    if (requestOrderUpdate.status) {
-        order.updateStatus(requestOrderUpdate.status);
-    }
-    if (requestOrderUpdate.discountCode) {
-        order.updateDiscountCode(requestOrderUpdate.discountCode);
-    }
-
-    await repo.save(order);
-    return `Order updated. New status: ${order.toDTO().status}`;
-}
-
-async function completeOrderUseCase(repo: OrderRepository, id: string) {
-    const order = await repo.findById(Id.from(id)) as Order;
-
-    if(!order) {
-        throw new DomainError(`Order not found to complete with id: ${id}`)
-    }
-    order.complete();
-    await repo.save(order);
-    return `Order with id ${order.toDTO().id} completed`;
-}
-
-async function deleteOrderUseCase(repo: OrderRepository, id: string) {
-    const order = await repo.findById(Id.from(id)) as Order;
-
-    if(!order) {
-        throw new DomainError('Order not found')
-    }
-    await repo.delete(order.getId())
-    return 'Order deleted';
-}
-
-export const getAllOrders = async (_req: Request, res: Response) => {
-    const repo = await Factory.getOrderRepository();
-    const ordersDTO = await getAllOrdersUseCase(repo);
-    res.json(ordersDTO);
-};
+import {OrderUseCase} from "../../application/orderUseCase";
 
 // Create a new order
 export const createOrder = async (req: Request, res: Response) => {
@@ -77,7 +9,7 @@ export const createOrder = async (req: Request, res: Response) => {
 
     try {
         const requestOrder = req.body;
-        let result = await createOrderUseCase(requestOrder, repo);
+        let result = await new OrderUseCase(repo).createOrder(requestOrder);
         res.send(result);
     } catch (error) {
         if (error instanceof DomainError) {
@@ -93,7 +25,7 @@ export const updateOrder = async (req: Request, res: Response) => {
 
     try {
         const requestOrderUpdate = {...req.body, id: req.params.id};
-        res.send(await updateOrderUseCase(repo, requestOrderUpdate));
+        res.send(await new OrderUseCase(repo).updateOrder(requestOrderUpdate));
     } catch (error) {
         if (error instanceof DomainError) {
             return res.status(404).send(error.message);
@@ -102,13 +34,20 @@ export const updateOrder = async (req: Request, res: Response) => {
     }
 };
 
+//Get All orders
+export const getAllOrders = async (_req: Request, res: Response) => {
+    const repo = await Factory.getOrderRepository();
+    const ordersDTO = await new OrderUseCase(repo).getAllOrders();
+    res.json(ordersDTO);
+};
+
 // Complete order
 export const completeOrder = async (req: Request, res: Response) => {
     const repo = await Factory.getOrderRepository();
 
     try {
         const { id } = req.params;
-        res.send(await completeOrderUseCase(repo, id));
+        res.send(await new OrderUseCase(repo).completeOrder(id));
     } catch (error) {
         if (error instanceof DomainError) {
             return res.status(400).send(error.message);
@@ -123,7 +62,7 @@ export const deleteOrder = async (req: Request, res: Response) => {
 
     try {
         const { id } = req.params;
-        let result = await deleteOrderUseCase(repo, id);
+        let result = await new OrderUseCase(repo).deleteOrder(id);
         res.send(result);
     }
     catch (error) {
